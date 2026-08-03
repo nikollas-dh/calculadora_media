6c6abc6bb3c375a535acba760e7a4bb5d751769f
= <nikollasdheyvis17@gmail.com>
2026-08-03 11:00:27 -0300

    iniciando nova primeira tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main (){
+  runApp(const MeuApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MeuApp extends StatelessWidget{
+  const MeuApp({super.key});
 
+@override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true
+      ),
+      home: MediaEscolarPage(),
+
+    );
+  }
+}
+
+class MediaEscolarPage extends StatefulWidget{
+  const MediaEscolarPage({super.key});
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+  State<MediaEscolarPage> createState() => _MediaEscolarPageState();
+}
+
+class _MediaEscolarPageState extends State<MediaEscolarPage>{
+  @override
+  Widget build (BuildContext context  ){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculadore de Média"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text("Minha primeira tela"),
       ),
     );
   }
